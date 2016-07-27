4516019608d1cea5d50e716ccc0b847388913d53
Patrick Fisher <patrick@pwfisher.com>
2016-07-27 10:01:34 -0700

    npm install

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-"use strict";
+'use strict';
+
+const RSVP = require('rsvp');
 const debounce = require('debounce');
+const exec = RSVP.denodeify(require('child_process').exec);
 const Sane = require('sane');
 
 const DEFAULT_DEBOUNCE_DELAY = 250;
@@ -12,10 +15,28 @@ class WatchNotifier {
   }
 
   subscribe(notify) {
+    this.notify = notify;
     const watcher = new Sane(this.distPath, this.saneOptions);
-    const listener = debounce(notify, this.debounceDelay);
+    const listener = debounce(this.restart.bind(this), this.debounceDelay);
     watcher.on('all', listener);
     return Promise.resolve();
+  }
+
+  restart() {
+    if (this.restartPromise) {
+      this.restartAgain = true;
+      return;
+    }
+    this.restartPromise = exec('npm install', { cwd: this.distPath })
+      .then(() => {
+        this.restartPromise = null;
+        if (this.restartAgain) {
+          this.restartAgain = false;
+          this.restart();
+        } else {
+          this.notify();
+        }
+      });
   }
 }
 
